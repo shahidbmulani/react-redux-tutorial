6fa7959e30326c9c6e2f6b012ce57fb7f1688274
Shahid Mulani <shahidm@techspawn.co>
2022-01-09 12:55:12 +0530

    initial commit

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import reducers from './reducers/index';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore( reducers, composeEnhancers( applyMiddleware( thunk ) ) );
+
+// const store = createStore(
+//     reducers,
+//     {},
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+// );
+
+export default store;
